f3ccf535f24032ecc53322c8e828886f3f88ed4e
Joao Vitor Dantas Pereira da Silva <joao.silva@dotzmkt.com.br>
2022-06-22 11:29:44 -0300

    buscacepcorreios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { CepServiceService } from './cep-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'BuscarCep';
+
+  constructor(private cepService: CepServiceService) {}
+
+  consultaCep(valor: any, form: any) {
+    this.cepService
+      .buscarCep(valor)
+      .subscribe((dados) => this.populaForm(dados, form));
+  }
+
+  populaForm(dados: any, form: any) {
+    form.setValue({
+      cep: dados.cep,
+      logradouro: dados.logradouro,
+      bairro: dados.bairro,
+      localidade: dados.localidade,
+      uf: dados.uf,
+    });
+  }
 }
